35afc5858d6667bd91ff36d0331c8a1e3e6a2017
Gabriela Guedes <zooiv3rde@gmail.com>
2019-07-14 17:31:27 -0300

    criando enum tipo cliente

--- src/main/java/dto/UserDto.java
+++ src/main/java/dto/UserDto.java
@@ -1,9 +1,31 @@
 package dto;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 public class UserDto implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @NotEmpty(message="Preenchimento obrigatório")
+    private String cpfOuCnpj;
 
+    private Integer tipo;
+
+    public UserDto() {}
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public Integer getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
 }
