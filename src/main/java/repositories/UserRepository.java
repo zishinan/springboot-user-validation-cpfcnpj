1d4f50ffb1e178e1722280895c01bae2fd570417
Gabriela Guedes <zooiv3rde@gmail.com>
2019-07-21 15:11:53 -0300

    ajuste na classe service

---
+++ src/main/java/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -1,10 +1,8 @@
 package services;
 
-import domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserService extends JpaRepository<User, Integer> {
+public class UserService {
 
 }
